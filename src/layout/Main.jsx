078d0c3d852b473a3b1974fcb91a7e52737cf09a
Shihab0 <ataharshihab1@gmail.com>
2022-12-24 05:30:02 +0600

    check git commands

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "../pages/Homepage/Home/Home";
+import Navbar from "../pages/Shared/Navbar";
 
 const Main = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Home></Home>
     </div>
   );
